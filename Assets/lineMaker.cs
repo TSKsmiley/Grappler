77a646f9b14ef9cc9b170b8c5b382a38d35f654f
BenjaminHvidberg <benjaminboegelhvidberg@gmail.com>
2019-09-19 16:26:54 +0200

    1st gen hook

--- Assets/lineMaker.cs
+++ Assets/lineMaker.cs
@@ -8,6 +8,8 @@ public class lineMaker : MonoBehaviour
 	LineRenderer lr;
     Rigidbody2D rb;
     public float speed;
+	SpringJoint2D sj;
+	Rigidbody2D currentBody;
 
 	public bool inTrigger = false;
 	bool grappleActive = false;
@@ -18,6 +20,7 @@ public class lineMaker : MonoBehaviour
     {
 		lr = GetComponent<LineRenderer>();
         rb = GetComponent<Rigidbody2D>();
+		sj = GetComponent<SpringJoint2D>();
 		lr.startWidth = 0F;
 		lr.endWidth = 0F;
 	}
@@ -31,42 +34,48 @@ public class lineMaker : MonoBehaviour
 		{
 			if (inTrigger == true)
 			{
+				sj.enabled = true;
 				grappleActive = true;
 				lr.startWidth = 0.1F;
 				lr.endWidth = 0.1F;
+				
 			}
 		}
 		if (Input.GetKey(KeyCode.Space))
 		{
 			lr.SetPosition(1, currentTrigger);
+			sj.connectedBody = currentBody;
 		}
 		if (Input.GetKeyUp(KeyCode.Space))
 		{
 			lr.startWidth = 0F;
 			lr.endWidth = 0F;
 			grappleActive = false;
+			
 		}
 
 		if (grappleActive == false)
 		{
 			lr.SetPosition(1, this.transform.position);
+			sj.enabled = false;
 		}
 	}
 
-    //void FixedUpdate()
-    //{
-    //    float moveHorizontal = Input.GetAxis("Horizontal");
-    //    float moveVertical = Input.GetAxis("Vertical");
+	//void FixedUpdate()
+	//{
+	//	float moveHorizontal = Input.GetAxis("Horizontal");
+	//	float moveVertical = Input.GetAxis("Vertical");
 
-    //    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-    //    rb.velocity = movement * speed;
-    //}
+	//	Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	//	rb.velocity = movement * speed;
+	//}
 
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Hook")
 		{
 			currentTrigger = other.transform.position;
+			currentBody = other.attachedRigidbody;
 			Debug.Log("Player Enterd Trigger");
 			inTrigger = true;
 		}
