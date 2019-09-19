7f860d221e13bf0aa7b7191d62a98bcbf8da0a48
FredTheNoob <43958385+FredTheNoob@users.noreply.github.com>
2019-09-19 19:43:22 +0200

    Fixed hooking issues

--- Assets/lineMaker.cs
+++ Assets/lineMaker.cs
@@ -7,12 +7,13 @@ public class lineMaker : MonoBehaviour
 
 	LineRenderer lr;
 	Rigidbody2D rb;
+    
 	public float speed;
 
-	//SpringJoint2D sj;
+	SpringJoint2D sj;
 	Rigidbody2D currentBody;
 
-	public bool inTrigger = false;
+	public int inTrigger = 0;
 	bool grappleActive = false;
 
 	Vector2 currentTrigger;
@@ -25,9 +26,8 @@ public class lineMaker : MonoBehaviour
 		lr.startWidth = 0F;
 		lr.endWidth = 0F;
 
-
-		//sj = GetComponent<SpringJoint2D>();
-	}
+        sj = GetComponent<SpringJoint2D>();
+    }
 
 
 	void Update()
@@ -36,28 +36,32 @@ public class lineMaker : MonoBehaviour
 
 		if (Input.GetKeyDown(KeyCode.Space))
 		{
-			if (inTrigger == true)
+			if (inTrigger > 0)
 			{
-				//sj.enabled = true;
-
+				sj.enabled = true;
 
 				grappleActive = true;
 				lr.startWidth = 0.1F;
 				lr.endWidth = 0.1F;
-
-				SpringJoint2D springTemp = gameObject.AddComponent(typeof(SpringJoint2D)) as SpringJoint2D;
-				springTemp.enableCollision = true;
-				springTemp.autoConfigureDistance = false;
-				springTemp.distance = 0.005F;
-				springTemp.dampingRatio = 1F;
 					
 			}
 		}
 		if (Input.GetKey(KeyCode.Space))
 		{
-			lr.SetPosition(1, currentTrigger);
+            if (inTrigger > 0)
+            {
+                sj.enabled = true;
 
-			//sj.connectedBody = currentBody;
+                grappleActive = true;
+                lr.startWidth = 0.1F;
+                lr.endWidth = 0.1F;
+
+            }
+
+
+            lr.SetPosition(1, currentTrigger);
+
+			sj.connectedBody = currentBody;
 		}
 		if (Input.GetKeyUp(KeyCode.Space))
 		{
@@ -65,35 +69,33 @@ public class lineMaker : MonoBehaviour
 			lr.endWidth = 0F;
 			grappleActive = false;
 
-			
-			
-		}
-
+            sj.connectedBody = null;
+        }
 		if (grappleActive == false)
 		{
 			lr.SetPosition(1, this.transform.position);
 
 
-			//sj.enabled = false;
+			sj.enabled = false;
 		}
 	}
 
-	//void FixedUpdate()
-	//{
-	//	float moveHorizontal = Input.GetAxis("Horizontal");
-	//	float moveVertical = Input.GetAxis("Vertical");
+    //void FixedUpdate()
+    //{
+    //    float moveHorizontal = Input.GetAxis("Horizontal");
+    //    float moveVertical = Input.GetAxis("Vertical");
 
-	//	Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-	//	rb.velocity = movement * speed;
-	//}
+    //    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+    //    rb.velocity = movement * speed;
+    //}
 
-	private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D other)
 	{
 		if (other.tag == "Hook")
 		{
 			currentTrigger = other.transform.position;
 			Debug.Log("Player Enterd Trigger");
-			inTrigger = true;
+			inTrigger += 1;
 
 			currentBody = other.attachedRigidbody;
 		}
@@ -104,7 +106,7 @@ public class lineMaker : MonoBehaviour
 		if (other.tag == "Hook")
 		{
 			Debug.Log("Player Left Trigger");
-			inTrigger = false;
+			inTrigger -= 1;
 		}
 	}
 }
