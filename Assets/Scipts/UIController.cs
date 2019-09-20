a288e144ef998d9a3b1cb2591e5e765115b67daf
FredTheNoob <frederikhammer02@gmail.com>
2019-09-20 10:48:25 +0200

    Added some UI elements, to start work on gamemenu

---
+++ Assets/Scipts/UIController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ButtonPlay()
+    {
+        
+    }
+
+    public void ButtonQuit()
+    {
+        Application.Quit(); 
+    }
+}
