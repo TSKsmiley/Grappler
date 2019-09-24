f894fe2b9da45e84bcde36ff6e0a95dd08636449
Mathias Frihauge <mathiasfrihauge@gmail.com>
2019-09-24 11:23:37 +0200

    Start screen added

---
+++ Assets/addforce.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addforce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.right * 1);
+    }
+}
