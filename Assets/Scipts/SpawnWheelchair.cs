6243825d0285e302c8d26b774f7e3431402a6079
BenjaminHvidberg <benjaminboegelhvidberg@gmail.com>
2019-09-24 15:04:03 +0200

    More level stuff lmao

---
+++ Assets/Scipts/SpawnWheelchair.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnWheelchair : MonoBehaviour
+{
+
+    public GameObject wheelchair;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+
+        Instantiate(wheelchair, transform.position, transform.rotation);
+        yield return new WaitForSeconds(10);
+        
+        StartCoroutine(Spawn());
+    }
+}
