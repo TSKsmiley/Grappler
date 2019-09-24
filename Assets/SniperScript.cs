4dde98ce70d1028b383719e59e73030b8e230195
FredTheNoob <frederikhammer02@gmail.com>
2019-09-24 12:59:52 +0200

    Started work on sniper script

---
+++ Assets/SniperScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SniperScript : MonoBehaviour
+{
+    LineRenderer lr;
+    public Rigidbody2D rbPlayer;
+    public Rigidbody2D rbSniper;
+
+    // Audio
+    public AudioSource AudioSrc;
+
+    public AudioClip ReloadSound;
+    public AudioClip SniperSound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
